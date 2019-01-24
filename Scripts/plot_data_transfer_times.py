3c0be0c35bb67d106edcad9fcf1557f2462f9c20
Bob Costas <finkzane@gmail.com>
2019-01-23 20:34:06 -0700

    added to_megabytes method

--- Scripts/plot_data_transfer_times.py
+++ Scripts/plot_data_transfer_times.py
@@ -18,26 +18,31 @@ def main():
 def parse_input( in_filename ):
     out_dict = {}
 
-    with open( in_filename, 'r' ) open_file:
+    with open( in_filename, 'r' ) as open_file:
         for line in open_file:
-            if 'DtoH' in line:
+            if 'HtoD' in line:
                 split_line = line.split()
 
                 byte_size     = to_megabytes( split_line[ 7 ] )
-                transfer_time = to_ms( split_line[ 7 ])
+                transfer_time = split_line[ 1 ]
 
                 if byte_size not in out_dict:
                     out_dict[ byte_size ] = list()
 
-                byte_size[ out_dict ].append( transfer_time )
+                out_dict[ byte_size ].append( transfer_time )
     return out_dict
 
-def to_ms( string_value ):
-    pass
 
 def to_megabytes( string_value ):
-    pass
-
+    if 'KB' in string_value:
+        value = float( string_value.split( 'KB' )[ 0 ] )
+        return value / 1000
+    elif 'MB' in string_value:
+        value = float( string_value.split( 'MB' )[ 0 ] )
+        return value
+    elif 'B' in string_value:
+        value = float( string_value.split( 'B' )[ 0 ] )
+        return value / ( 1000 * 1000 )
 
 if __name__ == '__main__':
     main()
