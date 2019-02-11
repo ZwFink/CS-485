c0ad9b623f8b8bf3533774bf3892a113551dcd12
Bob Costas <finkzane@gmail.com>
2019-02-10 17:16:44 -0700

    added command_args struct

--- Scripts/thrust_merge.cu
+++ Scripts/thrust_merge.cu
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#include <stdbool.h>
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
 #include <thrust/generate.h>
@@ -10,11 +11,19 @@
 using namespace std;
 
 const unsigned int DEVICE_CAPACITY_GB = 16;
+const unsigned int DEFAULT_NUM_ARGS   = 3; // 2 + 1, first is prog name
+
+typedef struct command_args
+{
+    unsigned int total_data_size;
+    unsigned int block_size;
+};
 
 int main( int argc, char **argv )
 {
-
+    
 
 
     return EXIT_SUCCESS;
 }
+
