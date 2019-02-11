d2e26427acce63a4c9068c197b0acb1b9d015f4a
Bob Costas <finkzane@gmail.com>
2019-02-10 17:09:54 -0700

    added imports and inclusions

---
+++ Scripts/thrust_merge.cu
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/generate.h>
+#include <thrust/copy.h>
+#include <thrust/merge.h>
+
+using namespace std;
+
+const unsigned int DEVICE_CAPACITY_GB = 16;
+
+int main( int argc, char **argv )
+{
+
+
+
+    return EXIT_SUCCESS;
+}
