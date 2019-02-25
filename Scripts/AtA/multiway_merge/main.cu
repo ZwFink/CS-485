c06f63110717c7a111fbc36233f03abd4033a5ba
Bob Costas <finkzane@gmail.com>
2019-02-24 21:41:53 -0700

    removed references to 'search', added K command line arg

--- Scripts/AtA/multiway_merge/main.cu
+++ Scripts/AtA/multiway_merge/main.cu
@@ -28,9 +28,9 @@
 
 // custom inclusions
 #include "omp.h"
-#include "mm_cpu.cpp"
-#include "mm_gpu.cu"
-#include "mm_utility.cu"
+#include "mm_cpu.h"
+#include "mm_gpu.h"
+#include "mm_utility.h"
 
 int main( int argc, char **argv )
 {
@@ -41,7 +41,7 @@ int main( int argc, char **argv )
 	////////////////
 	//Turn on gpu
 	printf("\nTurning on the GPU...\n");
-	warmUpGPU(0);
+	warm_up_gpu( 0 );
 	
 	/////////////////////////
 	// Get information from command line
@@ -50,10 +50,12 @@ int main( int argc, char **argv )
 	
 	//Read in parameters from file:
 	//dataset filename and cluster instance file
-	if (argc!=4)
+	if (argc!=5)
 	{
-	cout <<"\n\nIncorrect number of input parameters.  \nShould include a seed for the random number generator, the number of elements, N, and the batch size\n";
-	return 0;
+		printf( "\n\nIncorrect number of input parameters.  \nShould include a seed for the random number generator, "
+				"the number of elements, N, the number of sublists, K, and the batch size\n"
+		      );
+		return 0;
 	}
 	
 	//copy parameters from commandline:
@@ -76,6 +78,8 @@ int main( int argc, char **argv )
 	// uint64_t BATCHSIZE=atoi(inputBatchSize);
 	uint64_t BATCHSIZE = strtoull(inputBatchSize, NULL, 0);
 
+	uint8_t K = argv[ 4 ][ 0 ];
+
 	printf("\nSeed for random number generator: %d",seed);
 	printf("\nInput size: %lu",N);
 	printf("\nBatch size: %lu",BATCHSIZE);
@@ -89,36 +93,26 @@ int main( int argc, char **argv )
 	
 	//rng for the keys
 	std::mt19937 gen(seed); 
-	//rng for the searched values
-	std::mt19937 gen2(seed+74439); 
 	//transform the randomly generated numbers into uniform distribution of ints
 	std::uniform_int_distribution<uint64_t> dis(0, N);
 	
 
 	
 	//input to search
-	uint64_t * input;
-	input=new uint64_t[N];
-
-	// search values:
-	uint64_t * search;
-	search=new uint64_t[N];
+	uint64_t *input;
+	input = new uint64_t[N];
 
 	// result array:
-	uint64_t * result;
-	result=new uint64_t[N];	
+	uint64_t *result;
+	result = new uint64_t[N];	
 	
-	// uint64_t * result_idx;
-	// result_idx=new uint64_t[N];
 
-	for (uint64_t i=0; i<N; i++){
+	for (uint64_t i=0; i<N; i++)
+	{
 		input[i]=dis(gen);
-		search[i]=dis(gen2);
-		// totalInput+=(int)array[i];
 	}
 
 	printf("\nTotal size of input sorted array (MiB): %f",((double)N*(sizeof(uint64_t)))/(1024.0*1024.0));
-	printf("\nTotal size of input search array (MiB): %f",((double)N*(sizeof(uint64_t)))/(1024.0*1024.0));
 	printf("\nTotal size of result set array (MiB): %f",((double)N*(sizeof(uint64_t)))/(1024.0*1024.0));
 
 	
@@ -126,9 +120,8 @@ int main( int argc, char **argv )
 	//sort input array in parallel
 	double tstartsort=omp_get_wtime();
 	sortInputDataParallel(input, N);
-	sortInputDataParallel(search, N);
 	double tendsort=omp_get_wtime();
-	printf("\nTime to sort the input and search arrays in parallel (not part of performance measurements): %f",tendsort - tstartsort);
+	printf("\nTime to sort the input array in parallel (not part of performance measurements): %f",tendsort - tstartsort);
 	
 	//start hybrid CPU+GPU total time timer
 	double tstarthybrid=omp_get_wtime();
@@ -137,10 +130,8 @@ int main( int argc, char **argv )
 	//The number of batches should ensure that the input dataset is split at one point
 	//The input batch size is thus an approximation
 	std::vector<uint64_t> input_offsets;
-	std::vector<uint64_t> search_offsets;
 	uint64_t max_input_batch_size=0;
-	uint64_t max_search_batch_size=0;
-	compute_batches(N, input, search, &input_offsets, &search_offsets, BATCHSIZE, &max_input_batch_size, &max_search_batch_size);
+	//compute_batches( N, input, &input_offsets, BATCHSIZE, &max_input_batch_size );
 
 	//split the data between CPU and GPU for hybrid searches
 	unsigned int numCPUBatches=(input_offsets.size()-1)*CPUFRAC;
