f64b268c0df91797660dba5d669ec78775841e64
Bob Costas <finkzane@gmail.com>
2019-02-25 21:48:55 -0700

    removed warm_up_gpu

--- Scripts/AtA/multiway_merge/mm_utility.cpp
+++ Scripts/AtA/multiway_merge/mm_utility.cpp
@@ -15,8 +15,6 @@
 #include <thread>
 #include <utility>
 
-
-
 // // thrust inclusions
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
@@ -101,37 +99,6 @@
 
 
 // }
-
-void warm_up_gpu( int device )
-{
-    cudaSetDevice( device ); 		
-    // initialize all ten integers of a device_vector to 1 
-    thrust::device_vector<int> D(10, 1); 
-    // set the first seven elements of a vector to 9 
-    thrust::fill(D.begin(), D.begin() + 7, 9); 
-    // initialize a host_vector with the first five elements of D 
-    thrust::host_vector<int> H(D.begin(), D.begin() + 5); 
-    // set the elements of H to 0, 1, 2, 3, ... 
-    thrust::sequence(H.begin(), H.end()); // copy all of H back to the beginning of D 
-    thrust::copy(H.begin(), H.end(), D.begin()); 
-    // print D 
-
-    printf("\nDevice: %d\n",device);
-
-    for(int i = 0; i < D.size(); i++) 
-        std::cout << " D[" << i << "] = " << D[i]; 
-
-
-    // empty the vector
-    D.clear();
-
-    // deallocate any capacity which may currently be associated with vec
-    D.shrink_to_fit();
-
-    printf("\n");
-
-    return;
-}
 
 void generate_k_sorted_sublists( uint64_t *base_ptr, uint64_t total_elements, unsigned int seed, uint8_t k )
 {
--- Scripts/AtA/multiway_merge/mm_utility.h
+++ Scripts/AtA/multiway_merge/mm_utility.h
@@ -1,7 +1,6 @@
 #ifndef MULTIWAY_UTILITY_H_INCLUDED 
 #define MULTIWAY_UTILITY_H_INCLUDED 
 
-void warm_up_gpu( int device );
 void generate_k_sorted_sublists( uint64_t *base_ptr, uint64_t total_elements, unsigned int seed, uint8_t k );
 
 #endif // MULTIWAY_UTILITY_H_INCLUDED 
