64bb13fcfaa065802b15d41b93dce9bbfac02f7e
Bob Costas <finkzane@gmail.com>
2019-02-25 21:48:43 -0700

    added warm_up_gpu

--- Scripts/AtA/multiway_merge/mm_gpu.cu
+++ Scripts/AtA/multiway_merge/mm_gpu.cu
@@ -31,3 +31,34 @@
 #include "omp.h"
 
 
+void warm_up_gpu( int device )
+{
+    cudaSetDevice( device ); 		
+    // initialize all ten integers of a device_vector to 1 
+    thrust::device_vector<int> D(10, 1); 
+    // set the first seven elements of a vector to 9 
+    thrust::fill(D.begin(), D.begin() + 7, 9); 
+    // initialize a host_vector with the first five elements of D 
+    thrust::host_vector<int> H(D.begin(), D.begin() + 5); 
+    // set the elements of H to 0, 1, 2, 3, ... 
+    thrust::sequence(H.begin(), H.end()); // copy all of H back to the beginning of D 
+    thrust::copy(H.begin(), H.end(), D.begin()); 
+    // print D 
+
+    printf("\nDevice: %d\n",device);
+
+    for(int i = 0; i < D.size(); i++) 
+        std::cout << " D[" << i << "] = " << D[i]; 
+
+
+    // empty the vector
+    D.clear();
+
+    // deallocate any capacity which may currently be associated with vec
+    D.shrink_to_fit();
+
+    printf("\n");
+
+    return;
+}
+
--- Scripts/AtA/multiway_merge/mm_gpu.h
+++ Scripts/AtA/multiway_merge/mm_gpu.h
@@ -1,5 +1,7 @@
 #ifndef MM_GPU_HH_INCLUDED
 #define MM_GPU_HH_INCLUDED 
 
+void warm_up_gpu( int device );
+
 #endif // MM_GPU_HH_INCLUDED
 
