79268b54702812e194e2e168571b9ca5a7e11a5a
Bob Costas <finkzane@gmail.com>
2019-03-03 22:30:49 -0700

    fixed function signatures to use pointers

--- Scripts/AtA/multiway_merge/mm_utility.h
+++ Scripts/AtA/multiway_merge/mm_utility.h
@@ -6,8 +6,8 @@ void compute_batches( uint64_t N, uint64_t *input, std::vector<uint64_t> *batch_
 void compute_offsets( uint64_t *input, std::vector<uint64_t> *batch_offsets, 
                                 std::vector<uint64_t> *offset_list, uint64_t batch_index, 
                                                             uint16_t k, uint64_t sublist_size );
-void set_beginning_of_offsets( std::vector<uint64_t> begin_offset_list, uint64_t sublist_size, uint16_t k );
-void get_offset_beginning( std::vector<uint64_t> offset_list, std::vector<uint64_t> begin_list );
+void set_beginning_of_offsets( std::vector<uint64_t> *begin_offset_list, uint64_t sublist_size, uint16_t k );
+void get_offset_beginning( std::vector<uint64_t> *offset_list, std::vector<uint64_t> *begin_list );
 uint64_t get_start_index( std::vector<uint64_t> in_list, uint64_t k, uint64_t sublist_size );
 
 #endif // MULTIWAY_UTILITY_H_INCLUDED 
