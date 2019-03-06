3bcf83f2f906dbe6e7238109f5a69f098300a8ce
Bob Costas <finkzane@gmail.com>
2019-03-05 20:19:44 -0700

    generate_k_sorted_sublists now returns reference

--- Scripts/AtA/multiway_merge/mm_utility.cpp
+++ Scripts/AtA/multiway_merge/mm_utility.cpp
@@ -94,7 +94,7 @@ void compute_offsets( uint64_t *input, std::vector<uint64_t> *batch_offsets,
     }
 }
 
-std::vector<uint64_t*> generate_k_sorted_sublists( uint64_t *base_ptr, uint64_t total_elements, unsigned int seed, uint16_t k )
+std::vector<uint64_t*> *generate_k_sorted_sublists( uint64_t *base_ptr, uint64_t total_elements, unsigned int seed, uint16_t k )
 {
     uint64_t outer_index  = 0;
     uint64_t batch_size   = 0;
@@ -103,8 +103,8 @@ std::vector<uint64_t*> generate_k_sorted_sublists( uint64_t *base_ptr, uint64_t 
 
     uint64_t elements_per_list = total_elements / k;
 
-    std::vector<uint64_t*> list_ptrs;
-    list_ptrs.reserve( k );
+    std::vector<uint64_t*> *list_ptrs = new std::vector<uint64_t*>;
+    list_ptrs->reserve( k );
 
 	//rng for the keys
 	std::mt19937 gen( seed ); 
@@ -121,7 +121,7 @@ std::vector<uint64_t*> generate_k_sorted_sublists( uint64_t *base_ptr, uint64_t 
             __gnu_parallel::sort( base_ptr + ( batch_index * elements_per_list ),
                                   base_ptr + ( batch_index * elements_per_list ) + elements_per_list - 1
                                 );
-            list_ptrs.push_back( base_ptr + ( batch_index * elements_per_list ) );
+            list_ptrs->push_back( base_ptr + ( batch_index * elements_per_list ) );
         }
     return list_ptrs;
 }
