30e92dc297e38d6d3b4b3cb9f3f4a5cc12c47c93
Bob Costas <finkzane@gmail.com>
2019-03-13 22:30:54 -0700

    added test file

---
+++ Scripts/AtA/multiway_merge/test.cu
@@ -0,0 +1,39 @@
+// c++ inclusions
+#include <stdlib.h>
+#include <stdio.h>
+#include <random>
+#include <string.h>
+#include <fstream>
+#include <math.h>
+#include <iostream>
+#include <string>
+#include <queue>
+#include <iomanip>
+#include <set>
+#include <algorithm> 
+#include <thread>
+#include <cstdint>
+#include <utility>
+#include <vector>
+
+// thrust inclusions
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/sort.h>
+#include <thrust/device_ptr.h>
+#include <thrust/system/cuda/execution_policy.h> //for streams for thrust (added with Thrust v1.8)
+#include <thrust/binary_search.h>
+#include <thrust/execution_policy.h>
+
+// custom inclusions
+#include "omp.h"
+#include "mm_cpu.h"
+#include "mm_gpu.h"
+#include "mm_utility.h"
+
+
+int main( void )
+{
+
+    return EXIT_SUCCESS;
+}
