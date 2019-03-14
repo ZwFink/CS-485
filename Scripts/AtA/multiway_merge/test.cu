f6e81ba2a8f52963a4577697d059cb039aa4b95c
Bob Costas <finkzane@gmail.com>
2019-03-14 00:02:55 -0700

    started first test

--- Scripts/AtA/multiway_merge/test.cu
+++ Scripts/AtA/multiway_merge/test.cu
@@ -31,9 +31,43 @@
 #include "mm_gpu.h"
 #include "mm_utility.h"
 
+int test_copy_to_pinned_buffer( uint64_t *input, uint64_t N, uint64_t K )
+{
+    uint64_t BATCH_SIZE = 1000000;
+    uint64_t *pinned_buff = nullptr;
+    cudaMallocHost( (void**) &pinned_buff, sizeof( uint64_t ) * BATCH_SIZE * 8 );
+
+    uint64_t index = 0;
+    for( index = 0; index < K; index++ )
+        {
+
+        }
+}
+copied_this_round += copy_to_pinned_buffer( input,
+                                                                              input_to_gpu_pinned,
+                                                                              start_index_gpu,
+                                                                              end_index_gpu,
+                                                                              thread_id,
+                                                                              BATCH_SIZE
+                                                                            );
 
 int main( void )
 {
 
+    uint64_t N = 1000000000;
+    int seed   = 41;
+    uint64_t K = 31250;
+       
+
+    uint64_t *input      = ( uint64_t * ) malloc( sizeof( uint64_t ) * N );
+    uint64_t *output_arr = (uint64_t *) malloc( sizeof( uint64_t ) * N );
+
+
+    list_begin_ptrs = *generate_k_sorted_sublists( input, N, seed, K );
+
+    assert( test_copy_to_pinned_buffer( input, N, K ) != 0 );
+
+    free( input );
+    free( output_arr );
     return EXIT_SUCCESS;
 }
