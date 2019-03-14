d2b6d79c9e1534f9cfc7f5f3833a18a5bb9ec927
Jordan Wright <jaw566@nau.edu>
2019-03-14 00:15:24 -0700

    script to run CPU trials

---
+++ Scripts/AtA/multiway_merge/run_main.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+./main 42 10000000000 16 4 >> outfile1.tsv
+
+./main 42 10000000000 16 8 >> outfile2.tsv
+
+./main 42 10000000000 16 16 >> outfile3.tsv
+
+./main 42 10000000000 16 24 >> outfile4.tsv
+
+./main 42 10000000000 16 32 >> outfile5.tsv
+
