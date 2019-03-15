f16d187b0e803e013fa580c2a95ee93cad096415
Jordan Wright <jaw566@nau.edu>
2019-03-15 10:36:58 -0700

    changed script args

--- Scripts/AtA/multiway_merge/run_main.sh
+++ Scripts/AtA/multiway_merge/run_main.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-./main 42 10000000000 16 4 >> outfile1.tsv
+./main 42 10000000000 400 4 > outfile.tsv
 
-./main 42 10000000000 16 8 >> outfile2.tsv
+./main 42 10000000000 400 8 >> outfile.tsv
 
-./main 42 10000000000 16 16 >> outfile3.tsv
+./main 42 10000000000 400 16 >> outfile.tsv
 
-./main 42 10000000000 16 24 >> outfile4.tsv
+./main 42 10000000000 400 24 >> outfile.tsv
 
-./main 42 10000000000 16 32 >> outfile5.tsv
+./main 42 10000000000 400 32 >> outfile.tsv
 
