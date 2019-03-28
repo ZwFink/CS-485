b005f4b29371f449313019c69437333fdc4caad5
Bob Costas <finkzane@gmail.com>
2019-03-27 19:55:28 -0700

    added trials

--- Scripts/AtA/multiway_merge/run_main.sh
+++ Scripts/AtA/multiway_merge/run_main.sh
@@ -1,12 +1,38 @@
 #!/bin/bash
+trials=6
 
-./main 42 10000000000 400 4 > outfile.tsv
+n=1000000000
+echo N="$n" >> outfile_2.tsv
+for((i=0; i<trials; i++)); do
+	./main 42 "$n" 312500 8 >> outfile_2.tsv
+done
 
-./main 42 10000000000 400 8 >> outfile.tsv
+echo \n\n >> outfile_2.tsv
+n=2000000000
+echo N="$n" >> outfile_2.tsv
+for((i=0; i<trials; i++)); do
+	./main 42 "$n" 625000 8 >> outfile_2.tsv
+done
 
-./main 42 10000000000 400 16 >> outfile.tsv
+echo \n\n >> outfile_2.tsv
 
-./main 42 10000000000 400 24 >> outfile.tsv
+n=3000000000
+echo N="$n" >> outfile_2.tsv
+for((i=0; i<trials; i++)); do
+	./main 42 "$n" 937500 8 >> outfile_2.tsv
+done
 
-./main 42 10000000000 400 32 >> outfile.tsv
+echo \n\n >> outfile_2.tsv
+n=4000000000
+echo N="$n" >> outfile_2.tsv
+for((i=0; i<trials; i++)); do
+	./main 42 "$n" 1250000 8 >> outfile_2.tsv
+done
+
+echo \n\n >> outfile_2.tsv
+n=5000000000
+echo N="$n" >> outfile_2.tsv
+for((i=0; i<trials; i++)); do
+	./main 42 "$n" 1562500 8 >> outfile_2.tsv
+done
 
